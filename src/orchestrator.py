3506fe158a39be5c2e9babccbc86faf68e176111
Petro Knysh <peteknysh@gmail.com>
2018-04-03 22:42:12 -0400

    removed redundant files and code

--- src/orchestrator.py
+++ src/orchestrator.py
@@ -1,20 +1,55 @@
+import socket
 import numpy
+import sys
 from scikits.audiolab import wavread
 
 
 class Orchestrator:
-    def __init__(self, audio):
+    def __init__(self, audio, microphone_amount, server_address, server_port):
         self.audio = audio
+        self.server_address = server_address
+        self.server_port = server_port
+        self.microphone_amount = microphone_amount
+        self.microphone_proxies = []
+        self.microphone_data = []
+        self.wave = []
 
     def handle_file_data(self):
 
         # removing header and second channel data
-        self.wave = wavread(self.audio)[0]
-        self.wave = [list(pair) for pair in self.wave]
-        audio_data = numpy.array(self.wave)
-        self.wave = list(audio_data.flatten())
-        self.wave = self.wave[::2]
-        self.wave = numpy.array(self.wave).reshape(-1, 1)
+        wave = wavread(self.audio)[0]
+        wave = [list(pair) for pair in wave]
+        audio_data = numpy.array(wave)
+        wave = list(audio_data.flatten())
+        wave = wave[::2]
+        wave = numpy.array(wave).reshape(-1, 1)
 
-        self.scale = 0.8 / max(self.wave)
-        self.wave = numpy.multiply(self.scale, self.wave)
+        scale = 0.8 / max(wave)
+        self.wave = numpy.multiply(scale, wave)
+
+    def spawn_microphones(self):
+        for i in range(self.microphone_amount):
+            self.send_data_via_udp(self.microphone_data[i])
+
+    def send_data_via_udp(self, message):
+
+        # Create a UDP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        server_address = (self.server_address, self.server_port)
+        # server_address = ('localhost', 10000)
+        # message = 'This is the message.  It will be repeated.'
+
+        try:
+            # Send data
+            print >> sys.stderr, 'sending "%s"' % message
+            sent = sock.sendto(message, server_address)
+
+            # Receive response
+            print >> sys.stderr, 'waiting to receive'
+            data, server = sock.recvfrom(4096)
+            print >> sys.stderr, 'received "%s"' % data
+
+        finally:
+            print >> sys.stderr, 'closing socket'
+            sock.close()
