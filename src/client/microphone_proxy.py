b1f65ac55c90f1ecdab523f99127d5ba43757c69
Petro Knysh <peteknysh@gmail.com>
2018-04-09 23:17:41 -0400

    minor cleanings

--- src/client/microphone_proxy.py
+++ src/client/microphone_proxy.py
@@ -14,27 +14,20 @@ class MicrophoneProxy:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
         server_address = (self.__server_address, self.__server_port)
-        # message = 'This is the message.  It will be repeated.'
 
         try:
             # Send data
             logging.info("Sending data from microphone with id %s.", self.id)
 
-            # print >> sys.stderr, 'sending "%s"' % message
             sent = sock.sendto(self.message, server_address)
 
             # Receive response
-            # print >> sys.stderr, 'waiting to receive'
             logging.info("Data sent. Awaiting for response.")
 
             data, server = sock.recvfrom(4096)
 
             logging.info("Received server response.")
-            # print >> sys.stderr, 'received "%s"' % data
-
         finally:
-            # print >> sys.stderr, 'closing socket'
-            
             logging.info("Closing microphone's socket with id %s.", self.id)
 
             sock.close()
