ae2cdac9dc1ceb6c3d9fb7843ff3adbd7e1946cc
Petro Knysh <peteknysh@gmail.com>
2018-04-10 00:47:46 -0400

    wrok with server

--- src/server_dgram/server.py
+++ src/server_dgram/server.py
@@ -14,6 +14,10 @@ class Server:
         self.__true_positions = true_positions
         self.__estimated_positions = estimated_positions
         self.__trials = trials
+        self.__sensor_positions = []
+        self.__distances = []
+        self.__time_delays = []
+        self.__padding = []
 
     def run(self):
 
@@ -42,9 +46,9 @@ class Server:
         for i in range(self.__trials):
             r = numpy.random.rand(1) * 50
             t = numpy.random.rand(1) * 2 * math.pi
-            #r = 0.1 * 50
-            #t = 0.2 * 50
-            #z = 0.3 * 20
+            # r = 0.1 * 50
+            # t = 0.2 * 50
+            # z = 0.3 * 20
             x = r * math.cos(t)
             y = r * math.sin(t)
             z = numpy.random.rand(1) * 20
@@ -53,6 +57,22 @@ class Server:
             self.__true_positions[i, 2] = z
 
         logging.info('Generated sources positions.')
+
+    def generate_distances(self):
+        logging.info('Generating distances.')
+
+        self.__distances = numpy.zeros((self.__trials, self.__microphone_amount))
+        for i in range(self.__trials):
+            for j in range(self.__microphone_amount):
+                x1 = self.__true_positions[i, 0]
+                y1 = self.__true_positions[i, 1]
+                z1 = self.__true_positions[i, 2]
+                x2 = self.__sensor_positions[j, 0]
+                y2 = self.__sensor_positions[j, 1]
+                z2 = self.__sensor_positions[j, 2]
+                self.__distances[i, j] = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
+
+        logging.info('Generated distances.')
 
     def log_results(self):
         for trial_number in range(self.__trials):
@@ -63,13 +83,15 @@ class Server:
                          float(self.__estimated_positions[trial_number][1]),
                          float(self.__estimated_positions[trial_number][2]))
 
-            logging.info('True X = %.15f, True Y = %.15f, True Z = %.15f', float(self.__true_positions[trial_number][0]),
+            logging.info('True X = %.15f, True Y = %.15f, True Z = %.15f',
+                         float(self.__true_positions[trial_number][0]),
                          float(self.__true_positions[trial_number][1]),
                          float(self.__true_positions[trial_number][2]))
 
     def draw_plot(self):
         pyplot.plot(self.__true_positions[:, 0], self.__true_positions[:, 1], 'bd', label='True position')
-        pyplot.plot(self.__estimated_positions[:, 0], self.__estimated_positions[:, 1], 'r+', label='Estimated position')
+        pyplot.plot(self.__estimated_positions[:, 0], self.__estimated_positions[:, 1], 'r+',
+                    label='Estimated position')
         pyplot.legend(loc='upper right', numpoints=1)
         pyplot.xlabel('X coordinate of target')
         pyplot.ylabel('Y coordinate of target')
